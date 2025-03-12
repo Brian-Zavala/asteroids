6e37532d67433e9524b7bf9bcc3fd4f5b9bbafd0
Brian-Zavala <brian.zavala25@proton.me>
2025-03-12 17:24:50 -0500

    added shots_group to Player class

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-from circleshape import CircleShape
+from circleshape import CircleShape, Shot
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -20,7 +21,7 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-    def rotate(self, dt):
+    def rotate(self, dt, shots_group):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
@@ -34,7 +35,26 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(shots_group)
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self, shots_group):
+
+        # Create velocity vector (starts pointing down)
+        shot_velocity = pygame.Vector2(0, 1)
+
+        # Rotate to match player direction
+        shot_velocity = shot_velocity.rotate((self.rotation))
+
+        # Scale up by PLAYER_SHOOT_SPEED
+        shot_velocity = shot_velocity * PLAYER_SHOOT_SPEED
+
+        # Create new shot at player position
+        new_shot = Shot(self.position.x, self.position.y, shot_velocity)
+
+        # Add to shots group (you'll beed to pass this group to the method or access it some other way)
+        shots_group.add(new_shot)
